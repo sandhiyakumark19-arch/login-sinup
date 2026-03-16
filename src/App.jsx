1836547afbdbe10844cb707f38ce6723a14bcedb
sandhiyakumark19-arch <kumarkumar06101982@gmail.com>
2026-03-16 14:22:09 +0530

    fix: router structure for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
