43e37597c74d8d8082347b09b3eddb3285582cd2
sandhiyakumark19-arch <kumarkumar06101982@gmail.com>
2026-03-16 14:06:20 +0530

    fix: use HashRouter for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/Home";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
 function App() {
   return (
-  
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        {/* Handle old/incorrect URLs */}
-        <Route path="/login-sinup/*" element={<Navigate to="/" replace />} />
-        {/* Fallback */}
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+    </HashRouter>
   );
 }
 
